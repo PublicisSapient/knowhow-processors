a88a1721e9e576353d64abee53c3ab341a7baed4
gipathak <76935084+gipathak@users.noreply.github.com>
2025-07-23 13:31:43 +0530

    Revert "DTS-47969: Defect severity index changes"

--- jira/src/main/java/com/publicissapient/kpidashboard/jira/processor/JiraIssueHistoryProcessorImpl.java
+++ jira/src/main/java/com/publicissapient/kpidashboard/jira/processor/JiraIssueHistoryProcessorImpl.java
@@ -296,8 +296,6 @@ public class JiraIssueHistoryProcessorImpl implements JiraIssueHistoryProcessor 
 		List<JiraHistoryChangeLog> workLog = getJiraFieldChangeLog(changeLogList, JiraConstants.WORKLOG);
 		List<JiraHistoryChangeLog> dueDateChangeLog = getDueDateChangeLog(changeLogList, fieldMapping, fields);
 		List<JiraHistoryChangeLog> devDueDateChangeLog = getDevDueDateChangeLog(changeLogList, fieldMapping, fields);
-		List<JiraHistoryChangeLog> severityChangeLog = getCustomFieldChangeLog(changeLogList,
-				handleStr(fieldMapping.getJiraDefectSeverityKPI194()), fields);
 		List<JiraHistoryChangeLog> sprintChangeLog = getCustomFieldChangeLog(changeLogList,
 				handleStr(fieldMapping.getSprintName()), fields);
 		List<JiraHistoryChangeLog> flagStatusChangeLog;
@@ -330,7 +328,6 @@ public class JiraIssueHistoryProcessorImpl implements JiraIssueHistoryProcessor 
 		jiraIssueCustomHistory.setSprintUpdationLog(sprintChangeLog);
 		jiraIssueCustomHistory.setFlagStatusChangeLog(flagStatusChangeLog);
 		jiraIssueCustomHistory.setWorkLog(workLog);
-		jiraIssueCustomHistory.setSeverityUpdationLog(severityChangeLog);
 	}
 
 	private List<JiraHistoryChangeLog> getDevDueDateChangeLog(List<ChangelogGroup> changeLogList,
